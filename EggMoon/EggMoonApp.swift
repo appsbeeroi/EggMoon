71fbbf3fbc6adbaa230ba78763870525909d956f
esplusnin <esplusnin94@yandex.ru>
2025-10-03 09:01:50 +0300

    Initial Commit

---
+++ EggMoon/EggMoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggMoonApp.swift
+//  EggMoon
+//
+//  Created by Евгений on 03.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct EggMoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
